cebecc02323ba648b2c4095013730adc338fdda0
Emauri Neto <emaurigomesneto@gmail.com>
2024-08-17 19:51:32 -0300

    [chore] add server routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import express from "express"
 import cors from 'cors'
 import cookieParser from "cookie-parser"
 import { CLIENT_URL, PORT } from "./config/getEnv"
+import authRoutes from "./routes/auth.routes"
 
 const app = express()
 app.use(express.json())
@@ -16,6 +17,8 @@ app.use(cors({
 app.get("/api/healthcheck", (req, res) => {
   res.send({ status: "OK" })
 })
+
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
